aa1c1f869d74effc56ce074e6c7e3730b553260d
josefkara <josef.karakoca@mindler.se>
2021-03-20 12:52:50 +0100

    some structure for selections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import logo from './logo.svg';
 import '@google/model-viewer';
 import './App.css';
+import React, { useState } from 'react';
+
+const TextHighlight = ({text, onClick, activeSelection}) => {
+  const isSelected = text === activeSelection 
+  return(
+    <div className="highlight"
+         id={isSelected && "highlightSelected"}
+         onClick={() => onClick(text)} 
+    >
+      <p>{text}</p>
+    </div>
+)}
+
+const HotSpot = ({text, selection}) => {
+  const dataPostions = {
+    "Cervical": "-0.54 0.93 0.1", 
+    "Thoracic": 1,
+    "Lumbar": 1 ,
+    "Sacrum": 1 ,
+    "Coccyx": 1,
+  }
+  return(
+    <button slot="hotspot-hand" data-position="-0.54 0.93 0.1" data-normal="-0.73 0.05 0.69">
+      <div id="annotation">{text}</div>
+    </button>
+  )
+}
 
 function App() {
+  const [activeSelection, setActiveSelection] = useState("");
+  const keyWords = ["Cervical", "Thoracic", "Lumbar", "Sacrum", "Coccyx"]
   return (
     <div className="App">
-
-
+      <div className="contentWrapper">
           <div className= "rowItem">
-            <h2>YOUR MRI REPORT</h2>
-            <p className="dangerous">Disc 6 internal tear</p>
-            <p className="warning">Disc 7 lumbic tear</p>
-            <p className="normal">Disc 4 normal</p>
+            <h2>Your MRI REPORT </h2>
+            {keyWords.map((word) => 
+            <TextHighlight 
+              onClick={(t) => setActiveSelection(t)}
+              text={word}
+              activeSelection={activeSelection}
+            />)
+            }
           </div>
-          <div  className= "rowItem">
-            <model-viewer className="modelDisplay" height="100vh" width="50vw"  src='spine2.glb' camera-controls auto-rotate/>
+          <div className= "rowItem">
+              <model-viewer className="modelDisplay" src='spine2.glb' camera-controls auto-rotate>
+                  <HotSpot text="text" selection={activeSelection}></HotSpot>
+                </model-viewer>
+            </div>
           </div>
-
-
     </div>
   );
 }
